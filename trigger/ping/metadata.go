37e797c0274263f6cc2ffd28bb4e6ac7c0b56ffb
Akshay Gadikar <akshayug@gmail.com>
2018-11-02 13:07:54 -0700

    Added password validation

--- trigger/ping/metadata.go
+++ trigger/ping/metadata.go
@@ -13,7 +13,7 @@ type Settings struct {
 	Version 	string 	`md:"version"`
 	AppVersion 	string 	`md:"appversion"`
 	AppDescription 	string 	`md:"appdescription"`
-	Password	strings	`md:"password"`
+	Password	string	`md:"password"`
 }
 
 type MemoryStats struct{
