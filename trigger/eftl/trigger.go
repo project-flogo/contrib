aa840120d17f0cbf62465acbe14f4f4372b69268
Akshay Gadikar <akshayug@gmail.com>
2018-11-16 00:20:42 -0800

    added action handler

--- trigger/eftl/trigger.go
+++ trigger/eftl/trigger.go
@@ -12,10 +12,8 @@ import (
 	"github.com/project-flogo/core/action"
 	"github.com/project-flogo/core/support/log"
 	"github.com/project-flogo/core/data/metadata"
-	condition "github.com/mashling/commons/lib/conditions"
 	"github.com/mashling/commons/lib/eftl"
 	"github.com/mashling/commons/lib/util"
-	opentracing "github.com/opentracing/opentracing-go"
 )
 
 const (
@@ -47,7 +45,8 @@ type Trigger struct {
 	metadata   *trigger.Metadata
 	runner     action.Runner
 	config     *trigger.Config
-	handlers   map[string]*OptimizedHandler
+	logger     log.Logger
+	//handlers   map[string]*OptimizedHandler
 	connection *eftl.Connection
 	stop       chan bool
 }
@@ -65,54 +64,22 @@ func (f *Factory) New(config *trigger.Config) (trigger.Trigger, error) {
 
 // Init implements trigger.Init
 func (t *Trigger) Initialize(ctx trigger.InitContext) error {
-	t.handlers = t.CreateHandlers()
+	for _, handler := range ctx.GetHandlers() {
+
+		s := &HandlerSettings{}
+		err := metadata.MapToStruct(handler.Settings(), s, true)
+		if err != nil {
+			return err
+		}
+		t.newActionHandler(handler)
+		//router.Handle(method, path, newActionHandler(t, handler))
+	}
+	//t.handlers = t.CreateHandlers()
 	return nil
 }
 
-// CreateHandlers creates handlers mapped to thier topic
-func (t *Trigger) CreateHandlers() map[string]*OptimizedHandler {
-	handlers := make(map[string]*OptimizedHandler)
-	fmt.Println("Handlers : ", t.config.Handlers)
-	for _, h := range t.config.Handlers {
-		fmt.Println("Single handler :", h)
-		tr := h.Settings[settingDest]
-		if tr == nil {
-			continue
-		}
-		actionArray := h.Actions
-		actID := ""
-		for _, act := range actionArray {
-			actID = act.Config.Id
-		}
-		dest := tr.(string)
-
-		handler := handlers[dest]
-		if handler == nil {
-			handler = &OptimizedHandler{}
-			handlers[dest] = handler
-		}
-					//constant replaced
-		if condition := h.Settings["Condition"]; condition != nil {
-			fmt.Println("Before dispatch actionID:", actID)
-			dispatch := &Dispatch{
-				actionID: actID  ,
-				condition:  condition.(string),
-				handlerCfg: h,
-			}
-			handler.dispatches = append(handler.dispatches, dispatch)
-		} else {
-			handler.defaultActionID = actID
-			handler.defaultHandlerCfg = h
-		}
-	}
-	fmt.Println("Handlers returning:", handlers)
-	return handlers
-}
-
-
-// Start implements ext.Trigger.Start
-func (t *Trigger) Start() error {
-	fmt.Println("Inside start")
+func (t *Trigger) newActionHandler(handler trigger.Handler){
+	fmt.Println("Inside Trigger action handler")
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
 	}
@@ -120,7 +87,7 @@ func (t *Trigger) Start() error {
 	if ca != "" {
 		certificate, err := ioutil.ReadFile(ca.(string))
 		if err != nil {
-			logger.Errorf("can't open certificate", err)
+			t.logger.Errorf("can't open certificate", err)
 			return err
 		}
 		pool := x509.NewCertPool()
@@ -129,7 +96,6 @@ func (t *Trigger) Start() error {
 			RootCAs: pool,
 		}
 	}
-
 	id := t.config.Settings[settingID]
 	user := t.config.Settings[settingUser]
 	password := t.config.Settings[settingPassword]
@@ -146,18 +112,16 @@ func (t *Trigger) Start() error {
 	errorsChannel := make(chan error, 1)
 	connectVal, err := eftl.Connect(url.(string), options, errorsChannel)
 	if err != nil {
-		logger.Errorf("connection failed: %s", err)
+		t.logger.Errorf("connection failed: %s", err)
 		return err
 	}
 	t.connection = connectVal
 	messages := make(chan eftl.Message, 1000)
-	for dest := range t.handlers {
-		matcher := fmt.Sprintf("{\"_dest\":\"%s\"}", dest)
-		_, err := t.connection.Subscribe(matcher, "", messages)
-		if err != nil {
-			logger.Errorf("subscription failed: %s", err)
-			return err
-		}
+	matcher := fmt.Sprintf("{\"_dest\":\"%s\"}", handler.Settings["dest"])
+	_, err = t.connection.Subscribe(matcher, "", messages)
+	if err != nil {
+		t.logger.Errorf("subscription failed: %s", err)
+		return err
 	}
 
 	t.stop = make(chan bool, 1)
@@ -169,26 +133,21 @@ func (t *Trigger) Start() error {
 				value := message["_dest"]
 				dest, ok := value.(string)
 				if !ok {
-					logger.Errorf("dest is required for valid message")
+					t.logger.Errorf("dest is required for valid message")
 					continue
 				}
-				handler := t.handlers[dest]
-				if handler == nil {
-					logger.Errorf("no handler for dest ", dest)
-					continue
-				}
+
 				value = message["content"]
 				fmt.Println("value :", value)
 				content, ok := value.([]byte)
 				if !ok {
 					content = []byte{}
 				}
-				fmt.Println("Handler val:", handler)
 				fmt.Println("Dest val:", dest)
 				fmt.Println("Content val:", content)
-				t.RunAction(handler, dest, content)
+				t.RunAction(content,handler)
 			case err := <-errorsChannel:
-				logger.Errorf("connection error: %s", err)
+				t.logger.Errorf("connection error: %s", err)
 			case <-t.stop:
 				fmt.Println("inside stop")
 				return
@@ -196,6 +155,11 @@ func (t *Trigger) Start() error {
 		}
 	}()
 
+}
+
+
+// Start implements ext.Trigger.Start
+func (t *Trigger) Start() error {
 	return nil
 }
 
@@ -211,85 +175,60 @@ func (t *Trigger) Stop() error {
 }
 
 // RunAction starts a new Process Instance
-func (t *Trigger) RunAction(handler *OptimizedHandler, dest string, content []byte) {
+func (t *Trigger) RunAction(content []byte, handler trigger.Handler) {
 	fmt.Println("Inside Runaction")
-	//logger.Infof("EFTL Trigger: Received request for id '%s'", t.config.Id)
+	t.logger.Infof("EFTL Trigger: Received request for id '%s'", t.config.Id)
 
-	span := Span{
-		Span: opentracing.StartSpan(dest),
-	}
-	defer span.Finish()
-	fmt.Println("dest :", dest)
 	fmt.Println("content :", string(content))
-	span.SetTag("dest", dest)
-	span.SetTag("content", string(content))
 
-	replyTo, data := t.constructStartRequest(content, span)
+
+	replyTo, data := t.constructStartRequest(content)
 	fmt.Println("data :", data)
-	//to do :::::
-	//startAttrs, err := t.metadata.Output(data, false)
-	//if err != nil {
-	//	span.Error("Error setting up attrs: %v", err)
-	//}
 
-	actionURI, handlerCfg := handler.GetActionID(string(content), span)
-	//action := action.Get(actionURI)
-	actionArray := handlerCfg.Actions
-	var actions action.Action
-	for _, act := range actionArray {
-		if (act.Config.Id == actionURI){
-			actions = act.Act
-			break
-		}
-	}
-
-	context := trigger.NewHandlerContext(context.Background(), handlerCfg)
-	replyData, err := t.runner.RunAction(context, actions, data)
+	replyData, err := handler.Handle(context.Background(), data)
 	if err != nil {
-		span.Error("Error starting action: %v", err)
+		t.logger.Errorf("Error starting action: %v", err)
 	}
-	logger.Debugf("Ran action: [%s]", actionURI)
-	span.SetTag("actionURI", actionURI)
+	t.logger.Debugf("Ran action: [%s]", actionURI)
 
 	if replyTo == "" {
 		return
 	}
 	reply, err := util.Marshal(replyData)
 	if err != nil {
-		span.Error("failed to marshal reply data: %v", err)
+		t.logger.Errorf("failed to marshal reply data: %v", err)
 		return
 	}
-	span.SetTag("replyTo", replyTo)
-	span.SetTag("reply", string(reply))
 	err = t.connection.Publish(eftl.Message{
 		"_dest":   replyTo,
 		"content": reply,
 	})
 	if err != nil {
-		span.Error("failed to send reply data: %v", err)
+		t.logger.Errorf("failed to send reply data: %v", err)
 	}
 }
 
-func (t *Trigger) constructStartRequest(message []byte, span Span) (string, map[string]interface{}) {
-	span.SetTag("message", string(message))
+func (t *Trigger) constructStartRequest(message []byte) (string, map[string]interface{}) {
 
+	out := &Output{}
+	out.Content = map[string]interface{}{}
 	var content map[string]interface{}
-	err := util.Unmarshal("", message, &content)
+	err := util.Unmarshal("", message, &out.Content)
 	if err != nil {
-		span.Error("Error unmarshaling message %s", err.Error())
+		t.logger.Errorf("Error unmarshaling message %s", err.Error())
 	}
 
 	replyTo := ""
-	pathParams := make(map[string]string)
-	queryParams := make(map[string]string)
+	out.PathParams = make(map[string]string)
+	out.QueryParams = make(map[string]string)
 
 	mime := ""
-	if value, ok := content[util.MetaMIME].(string); ok {
+	if value, ok := out.Content[util.MetaMIME].(string); ok {
 		mime = value
 	}
 	if mime == util.MIMEApplicationXML {
 		getRoot := func() map[string]interface{} {
-			body := content[util.XMLKeyBody]
+			body := out.Content[util.XMLKeyBody]
 			if body == nil {
 				return nil
 			}
@@ -345,137 +284,31 @@ func (t *Trigger) constructStartRequest(message []byte, span Span) (string, map[
 			replyTo = value
 			delete(root, "replyTo")
 		}
-		fill("pathParams", pathParams)
-		fill("queryParams", queryParams)
+		fill("pathParams", out.PathParams)
+		fill("queryParams", out.QueryParams)
 	} else {
-		if value, ok := content["replyTo"].(string); ok {
+		if value, ok := out.Content["replyTo"].(string); ok {
 			replyTo = value
 			delete(content, "replyTo")
 		}
 
-		if params, ok := content["pathParams"].(map[string]interface{}); ok {
+		if params, ok := out.Content["pathParams"].(map[string]interface{}); ok {
 			for k, v := range params {
 				if param, ok := v.(string); ok {
-					pathParams[k] = param
+					out.PathParams[k] = param
 				}
 			}
 			delete(content, "pathParams")
 		}
 
-		if params, ok := content["queryParams"].(map[string]interface{}); ok {
+		if params, ok := out.Content["queryParams"].(map[string]interface{}); ok {
 			for k, v := range params {
 				if param, ok := v.(string); ok {
-					queryParams[k] = param
+					out.QueryParams[k] = param
 				}
 			}
 			delete(content, "queryParams")
 		}
 	}
-
-	//ctx := opentracing.ContextWithSpan(context.Background(), span)
-
-	data := map[string]interface{}{
-		"params":      pathParams,
-		"pathParams":  pathParams,
-		"queryParams": queryParams,
-		"content":     content,
-		//"tracing":     ctx,
-	}
-
-	return replyTo, data
+	return replyTo, out
 }
-
-
-//OptimizedHandler optimized handler
-type OptimizedHandler struct {
-	defaultActionID   string
-	defaultHandlerCfg *trigger.HandlerConfig
-	dispatches        []*Dispatch
-}
-
-// Error is for reporting errors
-func (s *Span) Error(format string, a ...interface{}) {
-	str := fmt.Sprintf(format, a...)
-	s.SetTag("error", str)
-}
-
-// GetActionID gets the action id of the matched handler
-func (h *OptimizedHandler) GetActionID(payload string, span Span) (string, *trigger.HandlerConfig) {
-	fmt.Println("payload input :", payload)
-
-	actionID := ""
-	var handlerCfg *trigger.HandlerConfig
-
-	for _, dispatch := range h.dispatches {
-		expressionStr := dispatch.condition
-		//Get condtion and expression type
-		conditionOperation, exprType, err := condition.GetConditionOperationAndExpressionType(expressionStr)
-
-		if err != nil || exprType == condition.EXPR_TYPE_NOT_VALID {
-			span.Error("not able parse the condition '%v' mentioned for content based handler. skipping the handler.", expressionStr)
-			continue
-		}
-
-		logger.Debugf("Expression type: %v", exprType)
-		logger.Debugf("conditionOperation.LHS %v", conditionOperation.LHS)
-		logger.Debugf("conditionOperation.OperatorInfo %v", conditionOperation.OperatorInfo().Names)
-		logger.Debugf("conditionOperation.RHS %v", conditionOperation.RHS)
-
-		//Resolve expression's LHS based on expression type and
-		//evaluate the expression
-		if exprType == condition.EXPR_TYPE_CONTENT {
-			exprResult, err := condition.EvaluateCondition(*conditionOperation, payload)
-			if err != nil {
-				span.Error("not able evaluate expression - %v with error - %v. skipping the handler.", expressionStr, err)
-			}
-			if exprResult {
-				actionID = dispatch.actionID
-				handlerCfg = dispatch.handlerCfg
-			}
-		} else if exprType == condition.EXPR_TYPE_HEADER {
-			span.Error("header expression type is invalid for eftl trigger condition")
-		} else if exprType == condition.EXPR_TYPE_ENV {
-			//environment variable based condition
-			envFlagValue := os.Getenv(conditionOperation.LHS)
-			logger.Debugf("environment flag = %v, val = %v", conditionOperation.LHS, envFlagValue)
-			if envFlagValue != "" {
-				conditionOperation.LHS = envFlagValue
-				op := conditionOperation.Operator
-				exprResult := op.Eval(conditionOperation.LHS, conditionOperation.RHS)
-				if exprResult {
-					actionID = dispatch.actionID
-					handlerCfg = dispatch.handlerCfg
-				}
-			}
-		}
-
-		if actionID != "" {
-			logger.Debugf("dispatch resolved with the actionId - %v", actionID)
-			break
-		}
-	}
-
-	//If no dispatch is found, use default action
-	if actionID == "" {
-		actionID = h.defaultActionID
-		handlerCfg = h.defaultHandlerCfg
-		fmt.Println("ActionID :", actionID)
-		//logger.Debugf("dispatch not resolved. Continue with default action - %v", actionID)
-	}
-	for _, dispatch := range h.dispatches {
-		actionID = dispatch.actionID
-		handlerCfg = dispatch.handlerCfg
-		fmt.Println("actionID:", actionID)
-	}
-	//actionID = "microgateway:Pets"
-	//handlerCfg = h.defaultHandlerCfg
-	return actionID, handlerCfg
-}
-
-//Dispatch holds dispatch actionId and condition
-type Dispatch struct {
-	actionID   string
-	condition  string
-	handlerCfg *trigger.HandlerConfig
-}
-
