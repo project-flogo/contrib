480e371519b206f6db17a2fe776c40b0492b56b2
skothari-tibco <skothari@tibco.com>
2019-03-13 14:17:14 -0400

    Clean PR

--- trigger/cli/trigger_test.go
+++ trigger/cli/trigger_test.go
@@ -7,7 +7,7 @@ import (
 var jsonTestMetadata = getTestJsonMetadata()
 
 func getTestJsonMetadata() string {
-	jsonMetadataBytes, err := ioutil.ReadFile("./examples/single/flogo-single-cli.json")
+	jsonMetadataBytes, err := ioutil.ReadFile("trigger.json")
 	if err != nil {
 		panic("No Json Metadata found for trigger.json path")
 	}
