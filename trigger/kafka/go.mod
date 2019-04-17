d926135bc85b27f562684182856321834460365e
Frank Martinez <fmartinez@tibco.com>
2019-04-17 00:59:37 -0400

    update kafaka trigger

--- trigger/kafka/go.mod
+++ trigger/kafka/go.mod
@@ -2,7 +2,7 @@ module github.com/project-flogo/contrib/trigger/kafka
 
 require (
 	github.com/Shopify/sarama v1.22.0
-	github.com/julienschmidt/httprouter v1.2.0
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/project-flogo/core v0.9.0-beta.3
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.3.0
--- trigger/kafka/metadata.go
+++ trigger/kafka/metadata.go
@@ -5,20 +5,19 @@ import (
 )
 
 type Settings struct {
-	BrokerUrls string `md:"brokerUrls,required"`
-	User       string `md:"user"`
-	Password   string `md:"password"`
-	TrustStore string `md:"truststore"`
+	BrokerUrls string `md:"brokerUrls,required"` // The Kafka cluster to connect to
+	User       string `md:"user"`                // If connecting to a SASL enabled port, the user id to use for authentication
+	Password   string `md:"password"`            // If connecting to a SASL enabled port, the password to use for authentication
+	TrustStore string `md:"trustStore"`          // If connecting to a TLS secured port, the directory containing the certificates representing the trust chain for the connection. This is usually just the CACert used to sign the server's certificate
 }
 type HandlerSettings struct {
-	Topic     string `md:"topic,required"`
-	Partition string `md:"partition"`
-	Group     string `md:"group"`
-	OffSet    int64  `md:"offset"`
+	Topic      string `md:"topic,required"` // The Kafka topic on which to listen for messageS
+	Partitions string `md:"partitions"`     // The specific partitions to consume messages from
+	Offset     int64  `md:"offset"`         // The offset to use when starting to consume messages, default is set to Newest
 }
 
 type Output struct {
-	Message string `md:"string"`
+	Message string `md:"string"` // The message that was consumed
 }
 
 func (o *Output) ToMap() map[string]interface{} {
