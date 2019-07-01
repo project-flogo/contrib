68112307fe3377a66d57b85d47524cd9124bb070
Tracy Li <xli@tibco.com>
2019-07-01 11:41:28 -0500

    using  coerce.ToFloat64 function

--- function/string/float.go
+++ function/string/float.go
@@ -1,15 +1,12 @@
 package string
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/project-flogo/core/data"
 	"github.com/project-flogo/core/data/coerce"
+	"github.com/project-flogo/core/data/expression/function"
 	"github.com/project-flogo/core/support/log"
 	"math"
-	"strconv"
-
-	"github.com/project-flogo/core/data"
-	"github.com/project-flogo/core/data/expression/function"
 )
 
 func init() {
@@ -34,9 +31,9 @@ func (s *fnFloat) Eval(ins ...interface{}) (interface{}, error) {
 
 	log.RootLogger().Debugf("Start Float64 function with parameters %+v", ins)
 	if len(ins) == 1 {
-		return CoerceToFloat(ins[0])
+		return coerce.ToFloat64(ins[0])
 	} else if len(ins) == 2 {
-		val, err := CoerceToFloat(ins[0])
+		val, err := coerce.ToFloat64(ins[0])
 		if err != nil {
 			return 0, fmt.Errorf("Invalid float type [%+v]", ins[0])
 		}
@@ -63,24 +60,4 @@ func round(num float64) int {
 func getPrecisedFloat(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(round(num*output)) / output
-}
-
-func CoerceToFloat(val interface{}) (float64, error) {
-	switch t := val.(type) {
-	case int:
-		return float64(t), nil
-	case int64:
-		return float64(t), nil
-	case float64:
-		return float64(t), nil
-	case json.Number:
-		i, err := t.Float64()
-		return float64(i), err
-	case string:
-		return strconv.ParseFloat(t, 64)
-	case nil:
-		return 0.0, nil
-	default:
-		return 0.0, fmt.Errorf("Unable to coerce %#v to float64", val)
-	}
 }
