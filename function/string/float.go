8e8d2aa8ebf63bf96eecd4d1c2ee5e7f3d55e209
Tracy Li <xli@tibco.com>
2019-07-02 10:32:14 -0500

    Use pow10

--- function/string/float.go
+++ function/string/float.go
@@ -58,6 +58,6 @@ func round(num float64) int {
 }
 
 func getPrecisedFloat(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
+	output := math.Pow10(precision)
 	return float64(round(num*output)) / output
 }
