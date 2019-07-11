c1861e84644b1f22552bb505ec7c9c80f483f406
Tracy Li <xli@tibco.com>
2019-07-11 13:45:39 -0500

    support array append array

--- function/array/append_test.go
+++ function/array/append_test.go
@@ -21,6 +21,16 @@ func TestStaticAppend(t *testing.T) {
 	}
 }
 
+func TestAppendArray(t *testing.T) {
+	Original := []string{"Cat", "Dog", "Snake"}
+	target := []string{"Cat", "Dog", "Snake"}
+	expectedResult := []string{"Cat", "Dog", "Snake", "Cat", "Dog", "Snake"}
+	final, _ := s.Eval(Original, target)
+	for i, item := range final.([]string) {
+		assert.Equal(t, item, expectedResult[i])
+	}
+}
+
 func TestArrayEmtpy(t *testing.T) {
 	final, _ := s.Eval(nil, "Mouse")
 	for _, item := range final.([]string) {
