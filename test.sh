2e4479bffc94a295f48ac1ccfa0d0221d70e5ab4
skothari-tibco <skothari@tibco.com>
2020-01-23 11:10:16 -0500

    edit travis file to get correct result.

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #  List all the components in activity, trigger and function
 #  Append './' and run tests.
-for i in $(ls -d */*/ | awk '{dir=$0 ;pre="./";ldir= pre dir; print ldir }')
+val=true
+for i in $(ls -d */*/ | awk '{dir=$0 ;pre="/";home=ENVIRON["PWD"] ; ldir= home pre dir; print ldir }')
 do 
-    go test $i
+   cd $i
+   err=$(go test); echo $err
+    if [[ $err != *"PASS"* ]] ; then
+        val=false
+    fi    
 done 
+if [[ !$val ]]; then
+    exit 1
+fi 
