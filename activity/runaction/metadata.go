1b1749acf960456a787dc0ee4a0019097d9f8987
Frank Martinez <fmartinez@tibco.com>
2020-01-28 09:42:05 -0500

    fix runaction doc

--- activity/runaction/metadata.go
+++ activity/runaction/metadata.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Settings struct {
-	ActionRef      string                 `md:"actionRef,required"` //The ref to action
-	ActionSettings map[string]interface{} `md:"actionSettings,required"`
+	ActionRef      string                 `md:"actionRef,required"` // The 'ref' to the action to be run
+	ActionSettings map[string]interface{} `md:"actionSettings,required"` // The settings of the action
 }
 
 type Output struct {
-	Output map[string]interface{} `md:"output"`
+	Output map[string]interface{} `md:"output"` // The output of the action.
 }
 
 func (o *Output) ToMap() map[string]interface{} {
