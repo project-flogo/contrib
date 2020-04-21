c6d04a2eec2cc116f02d9c7591a6ee189286f56c
Tracy Li <xli@tibco.com>
2020-04-20 22:19:44 -0500

    add current to return datetime of now

---
+++ function/datetime/current_test.go
@@ -0,0 +1,21 @@
+package datetime
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/project-flogo/core/data/expression/function"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	function.ResolveAliases()
+}
+
+func TestCurrentDate_Eval(t *testing.T) {
+	n := CurrentDatetime{}
+	datetime, _ := n.Eval(nil)
+	fmt.Println(datetime)
+	assert.NotNil(t, datetime)
+}
+
