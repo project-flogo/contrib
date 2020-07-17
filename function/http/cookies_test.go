af345c4cc697a931c6e7807d9ea3aff9f0cf579d
Tracy Li <xli@tibco.com>
2020-07-17 15:27:13 -0500

    fix http function not working

--- function/http/cookies_test.go
+++ function/http/cookies_test.go
@@ -13,6 +13,54 @@ func TestRewriteCookies(t *testing.T) {
 	final, err := c.Eval(cookies, "tsc", "mashery.com", "/mashery-path/")
 	assert.Nil(t, err)
 	assert.NotNil(t, final)
-	expectedResult := []interface{}{"lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure", "tsc=UcRKVG68jC...; Domain=mashery.com; Path=/mashery-path/; Max-Age=86399; HttpOnly; Secure"};
+	expectedResult := []interface{}{"lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure", "tsc=UcRKVG68jC...; Domain=mashery.com; Path=/mashery-path/; Max-Age=86399; HttpOnly; Secure"}
 	assert.Equal(t, expectedResult, final)
+}
+
+func TestReqCookieToParams(t *testing.T) {
+	cookies := "lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure"
+	f := &fnReqCookiesToParams{}
+	final, err := f.Eval(cookies)
+	assert.Nil(t, err)
+	assert.Equal(t, "/", final.(map[string]string)["Path"])
+	assert.NotNil(t, final)
+}
+
+func TestReqCookieToParamsFromParam(t *testing.T) {
+	cookies := "lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure"
+	f := &fnReqCookiesToParams{}
+	final, err := f.Eval(cookies)
+	assert.Nil(t, err)
+	assert.Equal(t, "/", final.(map[string]string)["Path"])
+
+	f2 := &fnReqCookiesFromParams{}
+	value, err := f2.Eval(final)
+	assert.Nil(t, err)
+	assert.NotNil(t, value)
+}
+
+func TestReqCookieToObject(t *testing.T) {
+	cookies := "lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure"
+	f := &fnResCookieToObject{}
+	final, err := f.Eval(cookies)
+	assert.Nil(t, err)
+	assert.NotNil(t, final)
+
+	f2 := &fnResCookieFromObject{}
+	value, err := f2.Eval(final)
+	assert.Nil(t, err)
+	assert.NotNil(t, value)
+}
+
+func TestResCookiesToObjectMap(t *testing.T) {
+	cookies := "lo=; Path=/; Expires=Wed, 01 Jan 1800 00:00:00 GMT; Secure"
+	f := &fnResCookiesToObjectMap{}
+	final, err := f.Eval(cookies)
+	assert.Nil(t, err)
+	assert.NotNil(t, final)
+
+	f2 := &fnResCookiesFromObjectMap{}
+	value, err := f2.Eval(final)
+	assert.Nil(t, err)
+	assert.NotNil(t, value)
 }
