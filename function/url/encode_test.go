ecc6963c687af48a1dafb696a714227edf89831e
Abhijit Wakchaure <awakchau@tibco.com>
2021-07-07 17:26:48 +0530

    updated test case for url encode to include path escaping as well

--- function/url/encode_test.go
+++ function/url/encode_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestFnEncode(t *testing.T) {
 	f := &fnEncode{}
-	input := "https://subdomain.example.com/path?q=hello world#fragment with space"
+	input := "https://subdomain.example.com/path to be escaped?q=hello world#fragment with space"
 	v, err := f.Eval(input)
 	assert.Nil(t, err)
-	expected := "https://subdomain.example.com/path?q=hello+world#fragment%20with%20space"
+	expected := "https://subdomain.example.com/path%20to%20be%20escaped?q=hello+world#fragment%20with%20space"
 	assert.Equal(t, expected, v)
 }
